ee71196a7350072cba4cd77da77ed4a132d38d98
vasreb <vasreborn@gmail.com>
2019-06-02 16:24:19 +0300

    Updates

---
+++ precache-manifest.3251ff9578406dd1ff1d670d5525c950.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d732f673b8ac66788b7ce665fea4c12",
+    "url": "/edu1/index.html"
+  },
+  {
+    "revision": "310c394ae1dc18002937",
+    "url": "/edu1/static/css/3.764ccc25.chunk.css"
+  },
+  {
+    "revision": "586adae8e2cea8002c56",
+    "url": "/edu1/static/css/main.839662ea.chunk.css"
+  },
+  {
+    "revision": "af2d69b6731b759ab191",
+    "url": "/edu1/static/js/0.8660d468.chunk.js"
+  },
+  {
+    "revision": "310c394ae1dc18002937",
+    "url": "/edu1/static/js/3.46e1094b.chunk.js"
+  },
+  {
+    "revision": "f6f7a0595376b1f35755",
+    "url": "/edu1/static/js/4.dfa38063.chunk.js"
+  },
+  {
+    "revision": "3a558d2bbea719bbb7fb",
+    "url": "/edu1/static/js/5.b41d1f61.chunk.js"
+  },
+  {
+    "revision": "3857140bcdff494e9c6b",
+    "url": "/edu1/static/js/6.499dba4a.chunk.js"
+  },
+  {
+    "revision": "f2f4448d10b7409c508a",
+    "url": "/edu1/static/js/7.1cee7942.chunk.js"
+  },
+  {
+    "revision": "586adae8e2cea8002c56",
+    "url": "/edu1/static/js/main.0aa2547f.chunk.js"
+  },
+  {
+    "revision": "02f7fef32ef6d83ea9f8",
+    "url": "/edu1/static/js/runtime~main.fdcf0399.js"
+  }
+]);
